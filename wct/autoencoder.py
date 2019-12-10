0011bcd61289871dc1ad6e340b474438e4fe0e0e
Mikołaj Rozwadowski <hejmsdz@users.noreply.github.com>
2019-12-10 15:24:23 +0100

    pass an image through VGG without top layers

--- wct/autoencoder.py
+++ wct/autoencoder.py
@@ -1,7 +1,9 @@
-from tensorflow.keras.applications import VGG19 as encoder
+from tensorflow.keras.applications import VGG19
 
 # from tensorflow.keras.models import Model
 # from tensorflow.keras.layers import Input, UpSampling2D, Lambda
 
-e = encoder(include_top=False)
-e.summary()
+encoder = VGG19(include_top=False)
+
+if __name__ == "__main__":
+    encoder.summary()
