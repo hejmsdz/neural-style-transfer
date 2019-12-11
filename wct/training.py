b1ff82dd4466007f8741cb8476bbbc37eb4c13a9
Mikołaj Rozwadowski <hejmsdz@users.noreply.github.com>
2019-12-11 23:14:39 +0100

    add basic training code

---
+++ wct/training.py
@@ -0,0 +1,34 @@
+import glob
+import itertools
+import tensorflow as tf
+from tensorflow.keras.callbacks import ModelCheckpoint
+import numpy as np
+from .autoencoder import autoencoder
+from .img import imread
+
+def training_images(paths, batch_size):
+    batches = (paths[pos : pos + batch_size] for pos in range(0, len(paths), batch_size))
+    for batch in batches:
+        images = np.array([imread(path) for path in batch])
+        yield images, images # expected input and output for an auto-encoder are equal
+
+def save_checkpoints():
+    return ModelCheckpoint(
+        'models/autoencoder.h5',
+        monitor='loss',
+        save_best_only=True,
+        save_weights_only=True
+    )
+
+if __name__ == '__main__':
+    autoencoder.compile(optimizer='adam', loss='mse')
+    paths = glob.glob('images/*.png')
+    epochs = 1
+    batch_size = 3
+    options = {
+        'epochs': epochs,
+        'steps_per_epoch': len(paths) / batch_size,
+        'callbacks': [save_checkpoints()]
+    }
+    generator = training_images(paths, batch_size)
+    autoencoder.fit_generator(generator, **options)
