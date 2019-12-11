1cb3014a3961aa7ed9bcf190e025fe459c93c9bc
Mikołaj Rozwadowski <hejmsdz@users.noreply.github.com>
2019-12-11 23:47:04 +0100

    fix file reading error (constant path)

--- wct/img.py
+++ wct/img.py
@@ -2,7 +2,7 @@ import cv2
 import numpy as np
 
 def imread(filename):
-    return cv2.imread('images/valencia.png').astype(np.float32) / 255.0
+    return cv2.imread(filename).astype(np.float32) / 255.0
 
 def imshow(image, title='image'):
     cv2.imshow(title, image)
