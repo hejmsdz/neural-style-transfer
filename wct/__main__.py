704d3244326f3b4f7dec1448107a16e10a8cf8c3
Mikołaj Rozwadowski <hejmsdz@users.noreply.github.com>
2019-12-11 23:48:32 +0100

    display images processed by trained autoencoder

--- wct/__main__.py
+++ wct/__main__.py
@@ -1,8 +1,13 @@
 import numpy as np
+import os.path
 from .img import imread, imshow
 from .autoencoder import autoencoder
 
-im = imread('images/valencia.png')
+im = imread('images/cookies.png')
+
+weights_file = 'models/autoencoder.h5'
+if os.path.exists(weights_file):
+    autoencoder.load_weights(weights_file)
 
 out = autoencoder.predict(np.array([im]))[0]
 print(out.shape)
