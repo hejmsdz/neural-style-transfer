53a31ee91448df6de24d7eaee37fd305fbced46d
Mikołaj Rozwadowski <hejmsdz@users.noreply.github.com>
2019-12-11 22:20:32 +0100

    add reflection padding

--- wct/autoencoder.py
+++ wct/autoencoder.py
@@ -2,25 +2,29 @@ from tensorflow.keras import Model, Sequential
 from tensorflow.keras.layers import Input, Conv2D, UpSampling2D
 from tensorflow.keras.applications import VGG19
 
-encoder = VGG19(include_top=False)
+from .layers import ReflectingConv2D
+
+encoder = VGG19(include_top=False, input_shape=(224, 224, 3))
 encoder.trainable = False
 # encoder.summary()
 
 layer_output = encoder.get_layer('block3_conv1').output
 
 # block 3
-x = Conv2D(128, (3, 3), activation='relu', name='dec_block3_conv1')(layer_output)
+x = ReflectingConv2D(128, (3, 3), activation='relu', name='dec_block3_conv1')(layer_output)
 x = UpSampling2D(interpolation='nearest', name='dec_block3_upsample')(x)
-x = Conv2D(128, (3, 3), activation='relu', name='dec_block3_conv2')(x)
+x = ReflectingConv2D(128, (3, 3), activation='relu', name='dec_block3_conv2')(x)
 
 # block 2
-x = Conv2D(64, (3, 3), activation='relu', name='dec_block2_conv1')(x)
+x = ReflectingConv2D(64, (3, 3), activation='relu', name='dec_block2_conv1')(x)
 x = UpSampling2D(interpolation='nearest', name='dec_block2_upsample')(x)
 
 # block 1
-x = Conv2D(64, (3, 3), activation='relu', name='dec_block1_conv1')(x)
-x = UpSampling2D(interpolation='nearest', name='dec_block1_upsample')(x)
+x = ReflectingConv2D(64, (3, 3), activation='relu', name='dec_block1_conv1')(x)
 
-autoencoder = Model(encoder.input, outputs=[layer_output, x])
+# output
+x = ReflectingConv2D(3, 3, activation=None, name='dec_output')(x)
+
+autoencoder = Model(encoder.input, outputs=[x])
 autoencoder.build((224, 224))
 autoencoder.summary()
