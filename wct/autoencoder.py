e229b6120e676d9793a804e41b436fb0817fdc6b
Mikołaj Rozwadowski <hejmsdz@users.noreply.github.com>
2019-12-11 16:44:07 +0100

    architecture for decoder on block 3

--- wct/autoencoder.py
+++ wct/autoencoder.py
@@ -1,3 +1,5 @@
+from tensorflow.keras import Sequential
+from tensorflow.keras.layers import Input, Conv2D, UpSampling2D
 from tensorflow.keras.applications import VGG19 as encoder
 
 # from tensorflow.keras.models import Model
@@ -5,3 +7,25 @@ from tensorflow.keras.applications import VGG19 as encoder
 
 e = encoder(include_top=False)
 e.summary()
+
+decoder = Sequential()
+
+
+decoder.add(Input(shape=(None, None, 256)))
+
+# hook into relu_3_1
+# block 3
+decoder.add(Conv2D(128, (3, 3), activation='relu'))
+decoder.add(UpSampling2D(interpolation='nearest'))
+decoder.add(Conv2D(128, (3, 3), activation='relu'))
+
+# block 2
+decoder.add(Conv2D(64, (3, 3), activation='relu'))
+decoder.add(UpSampling2D(interpolation='nearest'))
+
+# block 1
+decoder.add(Conv2D(64, (3, 3), activation='relu'))
+
+decoder.build()
+
+decoder.summary()
