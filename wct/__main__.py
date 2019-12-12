31134e3135333d00404fe7fd3695921ea830f82f
Mikołaj Rozwadowski <hejmsdz@users.noreply.github.com>
2019-12-12 01:13:55 +0100

    test on a sequence of test images

--- wct/__main__.py
+++ wct/__main__.py
@@ -1,15 +1,14 @@
 import numpy as np
 import os.path
+import glob
 from .img import imread, imshow
 from .autoencoder import autoencoder
-
-im = imread('images/cookies.png')
 
 weights_file = 'models/autoencoder.h5'
 if os.path.exists(weights_file):
     autoencoder.load_weights(weights_file)
 
-out = autoencoder.predict(np.array([im]))[0]
-print(out.shape)
-
-imshow(out, 'autoencoder output')
+for path in glob.glob('images/*.png'):
+    im = imread(path)
+    out = autoencoder.predict(np.array([im]))[0]
+    imshow(np.column_stack([im, out]), 'autoencoder output')
