4040d94b1757b6c0b93a6d376d0634496ba8efb2
Mikołaj Rozwadowski <hejmsdz@users.noreply.github.com>
2020-01-28 18:10:03 +0100

    add names to masking layers

--- wct/layers/unpooling.py
+++ wct/layers/unpooling.py
@@ -4,18 +4,18 @@ import keras.backend as K
 def MaskedMaxPooling2D(*args, **kwargs):
     def apply_layer(x):
         output = MaxPooling2D(*args, **kwargs)(x)
-        mask = mask_make(output, x)
+        mask = mask_make(output, x, kwargs['name'])
         return output, mask
     return apply_layer
 
 def Unpooling2D(mask, *args, **kwargs):
     def apply_layer(x):
         x = UpSampling2D(*args, **kwargs)(x)
-        x = Lambda(unpool)([x, mask])
+        x = Multiply(name=f"{kwargs['name']}_apply_mask")([mask, x])
         return x
     return apply_layer
 
-def mask_make(x, orig):
+def mask_make(x, orig, name):
     t = UpSampling2D()(x)
     _, a, b, c = orig.shape # TODO: make it work with fully convolutional architecture (unspecified input size)
     print("ORIGINAL:")
@@ -26,19 +26,14 @@ def mask_make(x, orig):
     print(xReshaped.shape)
     print("ORIG ENLARGED:")
     print(origReshaped.shape)
-    together = Concatenate(axis=-1)([origReshaped, xReshaped])
+    together = Concatenate(axis=-1, name=f"{name}_")([origReshaped, xReshaped])
     togReshaped = Reshape((2, a, b, c))(together)
     print("TOG RESHAPED:")
     print(togReshaped.shape)
 
-    bool_mask = Lambda(lambda t: K.greater_equal(t[:, 0], t[:, 1]))(togReshaped)
+    bool_mask = Lambda(lambda t: K.greater_equal(t[:, 0], t[:, 1]), name=f"{name}_bool_mask")(togReshaped)
 
-    mask = Lambda(lambda t: K.cast(t, dtype='float32'))(bool_mask)
-    # mask = Reshape((a,b,c))(mask)
+    mask = Lambda(lambda t: K.cast(t, dtype='float32'), name=f"{name}_float_mask")(bool_mask)
     print(mask.shape)
     return mask
 
-def unpool(args):
-    x, input_mask = args
-    # return keras.layers.multiply([input_mask, UpSampling2D()(x)])
-    return Multiply()([input_mask, x])
