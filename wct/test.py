ead36495ba63753d152e19d85b0d8b82a0c23634
Mikołaj Rozwadowski <hejmsdz@users.noreply.github.com>
2020-01-28 17:50:31 +0100

    draw models

--- wct/test.py
+++ wct/test.py
@@ -4,12 +4,16 @@ from keras.layers import Input
 from .encoder import encoder_layers
 from .decoder import decoder_layers
 
+from keras.utils import plot_model
+
 image = Input(shape=(224, 224, 3))
 for block in [1,2,3,4]:
     encoded, masks = encoder_layers(block)(image)
     decoded = decoder_layers(block, masks)(encoded)
     ae = Model(image, decoded)
     ae.summary()
+
+    plot_model(ae, to_file=f"model{block}.png")
     input()
     print(ae.predict(np.zeros((1, 224, 224, 3))).shape)
     input()
