ea0ebaa4b5f9178b2e2ff0abbf499636030227b6
Mikołaj Rozwadowski <hejmsdz@users.noreply.github.com>
2020-01-28 18:00:24 +0100

    add layer names

--- wct/decoder.py
+++ wct/decoder.py
@@ -10,24 +10,24 @@ def decoder_layers(block, masks):
             x = ReflectingConv2D(512, (3, 3), activation='relu', name='decoder_block4_conv3')(x)
             x = ReflectingConv2D(512, (3, 3), activation='relu', name='decoder_block4_conv2')(x)
             x = ReflectingConv2D(512, (3, 3), activation='relu', name='decoder_block4_conv1')(x)
-            x = Unpooling2D(masks[3], (2, 2))(x)
+            x = Unpooling2D(masks[3], (2, 2), name='decoder_block4_unpool')(x)
 
         if block >= 3:
             x = ReflectingConv2D(256, (3, 3), activation='relu', name='decoder_block3_conv4')(x)
             x = ReflectingConv2D(256, (3, 3), activation='relu', name='decoder_block3_conv3')(x)
             x = ReflectingConv2D(256, (3, 3), activation='relu', name='decoder_block3_conv2')(x)
             x = ReflectingConv2D(256, (3, 3), activation='relu', name='decoder_block3_conv1')(x)
-            x = Unpooling2D(masks[2], (2, 2))(x)
+            x = Unpooling2D(masks[2], (2, 2), name='decoder_block3_unpool')(x)
 
         if block >= 2:
             x = ReflectingConv2D(128, (3, 3), activation='relu', name='decoder_block2_conv2')(x)
             x = ReflectingConv2D(128, (3, 3), activation='relu', name='decoder_block2_conv1')(x)
-            x = Unpooling2D(masks[1], (2, 2))(x)
+            x = Unpooling2D(masks[1], (2, 2), name='decoder_block2_unpool')(x)
 
         if block >= 1:
             x = ReflectingConv2D(64, (3, 3), activation='relu', name='decoder_block1_conv2')(x)
             x = ReflectingConv2D(64, (3, 3), activation='relu', name='decoder_block1_conv1')(x)
-            x = Unpooling2D(masks[0], (2, 2))(x)
+            x = Unpooling2D(masks[0], (2, 2), name='decoder_block1_unpool')(x)
         return x
     
     return apply_layers
