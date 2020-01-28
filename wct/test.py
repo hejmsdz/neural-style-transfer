98e82fbadf4ea73c4d508815675f4d5a0226b44c
Mikołaj Rozwadowski <hejmsdz@users.noreply.github.com>
2020-01-28 19:46:04 +0100

    use create_autoencder in test.py

--- wct/test.py
+++ wct/test.py
@@ -1,19 +1,11 @@
 import numpy as np
-from keras.models import Model
-from keras.layers import Input 
-from .encoder import encoder_layers
-from .decoder import decoder_layers
+from .autoencoder import create_autoencoder
 
 from keras.utils import plot_model
 
-image = Input(shape=(224, 224, 3))
-for block in [1,2,3,4]:
-    encoded, masks = encoder_layers(block)(image)
-    decoded = decoder_layers(block, masks)(encoded)
-    ae = Model(image, decoded)
+for block in [2]:
+    ae = create_autoencoder(block)
     ae.summary()
 
     plot_model(ae, to_file=f"model{block}.png")
-    input()
-    print(ae.predict(np.zeros((1, 224, 224, 3))).shape)
-    input()
+    print(ae.predict(np.zeros((1, 224, 224, 3))))
