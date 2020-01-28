1ff25cd37b2ad28653e9d585d390cd8e96cd2a82
Matthew Bartos <geekbajt@gmail.com>
2020-01-28 01:02:58 +0100

    Refactoring encoder

--- wct/layers.py
+++ wct/layers.py
@@ -63,65 +63,6 @@ def mask_make(x, orig):
     return mask
 
 
-def vgg_layers(inputs, target_layer):
-    masks = []
-    print("BLOCK 1:")
-    # Block 1
-    x = Conv2D(64, (3, 3), activation='relu', padding='same', name='block1_conv1')(inputs)
-    x = Conv2D(64, (3, 3), activation='relu', padding='same', name='block1_conv2')(x)
-    orig = x
-    x = MaxPooling2D((2, 2), strides=(2, 2), name='block1_pool')(x)
-    masks.append(mask_make(x, orig))
-    if target_layer == 1:
-        return x, masks
-
-    print("BLOCK 2:")
-    # Block 2
-    x = Conv2D(128, (3, 3), activation='relu', padding='same', name='block2_conv1')(x)
-    x = Conv2D(128, (3, 3), activation='relu', padding='same', name='block2_conv2')(x)
-    orig = x
-    x = MaxPooling2D((2, 2), strides=(2, 2), name='block2_pool')(x)
-    masks.append(mask_make(x, orig))
-    if target_layer == 2:
-        return x, masks
-
-    print("BLOCK 3:")
-    # Block 3
-    x = Conv2D(256, (3, 3), activation='relu', padding='same', name='block3_conv1')(x)
-    x = Conv2D(256, (3, 3), activation='relu', padding='same', name='block3_conv2')(x)
-    x = Conv2D(256, (3, 3), activation='relu', padding='same', name='block3_conv3')(x)
-    x = Conv2D(256, (3, 3), activation='relu', padding='same', name='block3_conv4')(x)
-    orig = x
-    x = MaxPooling2D((2, 2), strides=(2, 2), name='block3_pool')(x)
-    masks.append(mask_make(x, orig))
-    if target_layer == 3:
-        return x, masks
-
-    print("BLOCK 4:")
-    # Block 4
-    x = Conv2D(512, (3, 3), activation='relu', padding='same', name='block4_conv1')(x)
-    x = Conv2D(512, (3, 3), activation='relu', padding='same', name='block4_conv2')(x)
-    x = Conv2D(512, (3, 3), activation='relu', padding='same', name='block4_conv3')(x)
-    x = Conv2D(512, (3, 3), activation='relu', padding='same', name='block4_conv4')(x)
-    orig = x
-    x = MaxPooling2D((2, 2), strides=(2, 2), name='block4_pool')(x)
-    masks.append(mask_make(x, orig))
-    if target_layer == 4:
-        return x, masks
-
-    print("BLOCK 5:")
-    # Block 5
-    x = Conv2D(512, (3, 3), activation='relu', padding='same', name='block5_conv1')(x)
-    x = Conv2D(512, (3, 3), activation='relu', padding='same', name='block5_conv2')(x)
-    x = Conv2D(512, (3, 3), activation='relu', padding='same', name='block5_conv3')(x)
-    x = Conv2D(512, (3, 3), activation='relu', padding='same', name='block5_conv4')(x)
-    orig = x
-    x = MaxPooling2D((2, 2), strides=(2, 2), name='block5_pool')(x)
-    masks.append(mask_make(x, orig))
-
-    return x, masks
-
-
 def load_weights(model):
     weights_path = tf.keras.utils.get_file(
         'vgg19_weights_tf_dim_ordering_tf_kernels_notop.h5',
@@ -149,12 +90,66 @@ def VGG19(input_tensor=None, input_shape=None, target_layer=1):
     VGG19, up to the target layer (1 for relu1_1, 2 for relu2_1, etc.)
     """
     if input_tensor is None:
-        inputs = Input(shape=input_shape)
+        inputs = Input(shape=input_shape, name='to_ty_chuju_nie_dzialasz')
     else:
-        inputs = Input(tensor=input_tensor, shape=input_shape)
+        inputs = Input(tensor=input_tensor, shape=input_shape, name='to_ty_chuju_nie_dziala')
 
-    layers, masks = vgg_layers(inputs, target_layer)
-    model = Model(inputs, layers, name='vgg19')
+    masks = []
+    print("BLOCK 1:")
+    # Block 1
+    x = Conv2D(64, (3, 3), activation='relu', padding='same', name='block1_conv1')(inputs)
+    x = Conv2D(64, (3, 3), activation='relu', padding='same', name='block1_conv2')(x)
+    orig = x
+    x = MaxPooling2D((2, 2), strides=(2, 2), name='block1_pool')(x)
+    masks.append(mask_make(x, orig))
+    # if target_layer == 1:
+    #     return x, masks
+
+    print("BLOCK 2:")
+    # Block 2
+    x = Conv2D(128, (3, 3), activation='relu', padding='same', name='block2_conv1')(x)
+    x = Conv2D(128, (3, 3), activation='relu', padding='same', name='block2_conv2')(x)
+    orig = x
+    x = MaxPooling2D((2, 2), strides=(2, 2), name='block2_pool')(x)
+    masks.append(mask_make(x, orig))
+    # if target_layer == 2:
+    #     return x, masks
+
+    print("BLOCK 3:")
+    # Block 3
+    x = Conv2D(256, (3, 3), activation='relu', padding='same', name='block3_conv1')(x)
+    x = Conv2D(256, (3, 3), activation='relu', padding='same', name='block3_conv2')(x)
+    x = Conv2D(256, (3, 3), activation='relu', padding='same', name='block3_conv3')(x)
+    x = Conv2D(256, (3, 3), activation='relu', padding='same', name='block3_conv4')(x)
+    orig = x
+    x = MaxPooling2D((2, 2), strides=(2, 2), name='block3_pool')(x)
+    masks.append(mask_make(x, orig))
+    # if target_layer == 3:
+    #     return x, masks
+
+    print("BLOCK 4:")
+    # Block 4
+    x = Conv2D(512, (3, 3), activation='relu', padding='same', name='block4_conv1')(x)
+    x = Conv2D(512, (3, 3), activation='relu', padding='same', name='block4_conv2')(x)
+    x = Conv2D(512, (3, 3), activation='relu', padding='same', name='block4_conv3')(x)
+    x = Conv2D(512, (3, 3), activation='relu', padding='same', name='block4_conv4')(x)
+    orig = x
+    x = MaxPooling2D((2, 2), strides=(2, 2), name='block4_pool')(x)
+    masks.append(mask_make(x, orig))
+    # if target_layer == 4:
+    #     return x, masks
+
+    print("BLOCK 5:")
+    # Block 5
+    x = Conv2D(512, (3, 3), activation='relu', padding='same', name='block5_conv1')(x)
+    x = Conv2D(512, (3, 3), activation='relu', padding='same', name='block5_conv2')(x)
+    x = Conv2D(512, (3, 3), activation='relu', padding='same', name='block5_conv3')(x)
+    x = Conv2D(512, (3, 3), activation='relu', padding='same', name='block5_conv4')(x)
+    orig = x
+    x = MaxPooling2D((2, 2), strides=(2, 2), name='block5_pool')(x)
+    masks.append(mask_make(x, orig))
+
+    model = Model(inputs, x, name='vgg19', trainable=False)
     load_weights(model)
     return model, masks
 
